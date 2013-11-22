12e8ef71480af793b43484f09a1466b69d60ed1e
David Bernard <david.bernard.31@gmail.com>
2013-11-22 13:02:21 +0100

    add the firts operations : MicroTemplate, findBaseUrl, loadDataSvgs

---
+++ lib/html_toolbox.dart
@@ -0,0 +1,33 @@
+library html_toolbox;
+
+import 'dart:html';
+import 'dart:async';
+
+part 'html_toolbox/micro_template.dart';
+
+String findBaseUrl() {
+  String location = window.location.pathname;
+  int slashIndex = location.lastIndexOf('/');
+  if (slashIndex < 0) {
+    return '/';
+  } else {
+    return location.substring(0, slashIndex);
+  }
+}
+
+Iterable<Future<Element>> loadDataSvgs(){
+  return document.querySelectorAll("[data-svg-src]").map((el){
+    var src = el.dataset["svgSrc"];
+    return HttpRequest.request(src, responseType : 'document').then((httpRequest){
+      var doc = httpRequest.responseXml;
+      var child = doc.documentElement.clone(true);
+      // to fill parent el and keep original ratio of the image
+      child.attributes.remove("width");
+      child.attributes.remove("height");
+      child.style.width = "100%";
+      child.style.height = "100%";
+      el.append(child);
+      return child;
+    });
+  }).toList(growable: false); // to list is called to force execution of the map function (the function map() is lazy in Dart)
+}
---
+++ lib/html_toolbox/micro_template.dart
@@ -0,0 +1,42 @@
+part of html_toolbox;
+
+const uriPolicyAll = const _UriPolicyAll();
+
+interpolate(String tmpl, Map kv) {
+  var from = new RegExp(r'\$\{([^}]*)\}');
+  return tmpl.replaceAllMapped(from, (x) => findValue(kv, x.group(1)));
+}
+
+findValue(Map kv, String k) {
+  if (kv.containsKey(k)) return kv[k];
+  var ks = k.split(".");
+  return ks.fold(kv, (v, k) => v[k]);
+}
+
+class _UriPolicyAll implements UriPolicy{
+  const _UriPolicyAll();
+  bool allowsUri(String uri) => true;
+}
+
+class MicroTemplate {
+  var _tmpl = null;
+  var _tmplParent = null;
+
+  /// eg : tmpl = querySelector("script.ach")
+  MicroTemplate(Element tmpl) {
+    _tmplParent = tmpl.parent;
+    //_tmplAchievements = _tmplAchievementsParent.innerHtml;
+    _tmpl = (tmpl.tagName.toLowerCase() == "script") ? tmpl.text : tmpl.outerHtml;
+    _tmplParent.setInnerHtml('');
+  }
+
+  apply(Iterable<Map> items, {NodeValidator validator, UriPolicy uriPolicy : uriPolicyAll}) {
+    if (validator == null) {
+      validator = new NodeValidator(uriPolicy: uriPolicy);
+    }
+    _tmplParent.setInnerHtml(
+      items.fold("", (acc, item) => acc + (interpolate(_tmpl, item))),
+      validator: validator
+    );
+  }
+}
